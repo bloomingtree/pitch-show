428697fc0798dd2a7e04170addda3bca92101fe8
孙珂昌 <7988588+sun-kechang@user.noreply.gitee.com>
2025-06-08 17:24:27 +0800

    [ADD]增加打赏码

--- src/components/SongPitch.vue
+++ src/components/SongPitch.vue
@@ -42,10 +42,10 @@
       </div>
     </div>
     <!-- 快捷键说明选项栏 -->
-    <div class="group fixed right-5 -top-72 min-w-72 shadow-lg shadow-inner p-3 bg-white rounded-md h-72 transition-all duration-300 z-10
+    <div class="group fixed right-5 top-[-31rem] min-w-72 shadow-lg shadow-inner p-3 bg-white rounded-md h-[31rem] transition-all duration-300 z-10
     hover:top-0 hover:z-20">
-      <div class="w-full h-full flex flex-col">
-        <h2 class="text-left font-bold">快捷键说明</h2>
+      <div class="w-full h-full flex flex-col items-center">
+        <h2 class="font-bold text-orange-400 text-xl">快捷键说明</h2>
         <div class="flex justify-center items-center mt-2">
           <div class="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">&lt;</div>
           <p class="ml-2 text-sm text-gray-500 dark:text-gray-400">向左移动2秒</p>
@@ -61,6 +61,10 @@
         <div class="flex justify-center items-center mt-2">
           <div class="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">Spacebar</div>
           <p class="ml-2 text-sm text-gray-500 dark:text-gray-400">播放/暂停</p>
+        </div>
+        <div class="justify-center items-center mt-2">
+          <image class="w-full h-60" src="/comeon.jpg" />
+          <p class="ml-2 text-sm text-gray-600 dark:text-gray-400 font-bold">你的赞助，是我更新的动力！</p>
         </div>
       </div>
       <div class="absolute -bottom-16 left-1/3 h-16 w-1/3 group-hover:scale-y-0 group-hover:-bottom-8 transition-all delay-200">
