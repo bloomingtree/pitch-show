c5c7f98c99a060ff8f35be3c58ea2b3c3abcb37e
孙珂昌 <7988588+sun-kechang@user.noreply.gitee.com>
2026-01-25 16:34:00 +0800

    [ADD]增加了一个国内站点的按钮

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -50,8 +50,8 @@
             {{ $t('navigationBar.separate') }}
           </router-link>
           
-          <router-link 
-            to="/about" 
+          <router-link
+            to="/about"
             class="nav-link"
             :class="{ 'active': $route.name === 'about' }"
           >
@@ -60,6 +60,18 @@
             </svg>
             {{ $t('navigationBar.about') }}
           </router-link>
+
+          <a
+            href="https://shiyin.notalabs.cn/"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="nav-link"
+          >
+            <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+            </svg>
+            国内站点
+          </a>
         </div>
         <!-- 语言切换 -->
         <div class="flex items-center  cursor-pointer">
@@ -122,8 +134,8 @@
             {{ $t('navigationBar.separate') }}
           </router-link>
           
-          <router-link 
-            to="/about" 
+          <router-link
+            to="/about"
             class="mobile-nav-link"
             :class="{ 'active': $route.name === 'about' }"
             @click="closeMobileMenu"
@@ -133,6 +145,19 @@
             </svg>
             {{ $t('navigationBar.about') }}
           </router-link>
+
+          <a
+            href="https://shiyin.notalabs.cn/"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="mobile-nav-link"
+            @click="closeMobileMenu"
+          >
+            <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+            </svg>
+            国内站点
+          </a>
         </div>
       </div>
     </div>
